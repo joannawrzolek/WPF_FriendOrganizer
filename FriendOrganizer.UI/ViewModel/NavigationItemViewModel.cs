a7833df13cb267eddb0f5c8027034deed91c826a
joanna.wrzolek <joanna.wrzolek@jeppesen.com>
2018-01-13 00:19:15 +0100

    Remove section in NavigationView; Style for the Navigation

--- FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationItemViewModel.cs
@@ -1,14 +1,25 @@
-﻿namespace FriendOrganizer.UI.ViewModel
+﻿using System;
+using System.Windows.Input;
+using FriendOrganizer.UI.Event;
+using Prism.Commands;
+using Prism.Events;
+
+namespace FriendOrganizer.UI.ViewModel
 {
 	public class NavigationItemViewModel : ViewModelBase
 	{
 		private string _displayMember;
+		private IEventAggregator _eventAggregator;
 
-		public NavigationItemViewModel(int id, string displayMember)
+		public NavigationItemViewModel(int id, string displayMember, IEventAggregator eventAggregator)
 		{
+			_eventAggregator = eventAggregator;
 			Id = id;
 			DisplayMember = displayMember;
+			OpenFriendDetailViewCommand = new DelegateCommand(OnOpenFriendDetailView);
 		}
+
+		
 
 		public int Id { get; }
 
@@ -20,6 +31,14 @@
 				_displayMember = value;
 				OnPropertyChanged();
 			}
+		}
+
+		public ICommand OpenFriendDetailViewCommand { get; }
+
+		private void OnOpenFriendDetailView()
+		{
+			_eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+				.Publish(Id);
 		}
 	}
 }
--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,10 +1,8 @@
-﻿using FriendOrganizer.Model;
-using FriendOrganizer.UI.Data;
+﻿
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using FriendOrganizer.UI.Event;
 using Prism.Events;
-using System;
 using System.Linq;
 using FriendOrganizer.UI.Data.Lookups;
 
@@ -15,7 +13,8 @@ namespace FriendOrganizer.UI.ViewModel
 		private IFriendLookupDataService _friendLookupService;
 		private IEventAggregator _eventAgrregator;
 
-		public NavigationViewModel(IFriendLookupDataService friendLookupService, IEventAggregator eventAggregator)
+		public NavigationViewModel(IFriendLookupDataService friendLookupService, 
+			IEventAggregator eventAggregator)
 		{
 			_friendLookupService = friendLookupService;
 			_eventAgrregator = eventAggregator;
@@ -35,29 +34,11 @@ namespace FriendOrganizer.UI.ViewModel
 			Friends.Clear();
 			foreach (var item in lookup)
 			{
-				Friends.Add(new NavigationItemViewModel(item.Id, item.DisplayMember));
+				Friends.Add(new NavigationItemViewModel(item.Id, item.DisplayMember, 
+					_eventAgrregator));
 			}
 		}
 
 		public ObservableCollection<NavigationItemViewModel> Friends { get; }
-
-		private NavigationItemViewModel _selectedFriend;
-
-		public NavigationItemViewModel SelectedFriend
-		{
-			get { return _selectedFriend; }
-			set
-			{
-				_selectedFriend = value;
-				OnPropertyChanged();
-				if (_selectedFriend != null)
-				{
-					_eventAgrregator.GetEvent<OpenFriendDetailViewEvent>()
-						.Publish(_selectedFriend.Id);
-				}
-
-			}
-		}
-
 	}
 }
